95bce63a56f960a29f7be5b862df8db0aa0d4459
cosyphp <cosyphp@aliyun.com>
2019-05-13 17:53:59 +0800

    隐藏页面底部区块

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,5 +1,5 @@
 <!-- Main Footer -->
-<footer class="main-footer">
+<footer class="main-footer hide">
     <!-- To the right -->
     <div class="pull-right hidden-xs">
         @if(config('admin.show_environment'))
